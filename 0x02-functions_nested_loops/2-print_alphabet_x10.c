7345735c9e7cf6763469229e01cd248d6ad9496b
Aagi1 <gillesakpinfa@gmail.com>
2023-06-20 13:22:26 -0700

    hdhhd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,20 +7,15 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++;
+	for (ch = 'a'; ch <= ‘z’; ch++)
+	{
+	_putchar(ch);
+	}
+_putchar('\n');
+i++;
 	}
 }
-
