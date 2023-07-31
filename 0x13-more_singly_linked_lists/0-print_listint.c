7ab9f6d0c139f1c4463670d65615afcf7206515c
Aagi1 <gillesakpinfa@gmail.com>
2023-07-31 13:37:45 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * prints all the elements of a listint_t list.
+ * @h: liste chaînée de type listint_t à imprimer
+ * Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t num = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
+
+
