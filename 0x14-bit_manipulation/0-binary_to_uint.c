8bfc7c441d94b333c613791bf52d79f68c84ecbd
Aagi1 <gillesakpinfa@gmail.com>
2023-09-29 01:08:12 -0700

    0x14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_uint - converti binaire en decimale
+ * @b: Le charactere qui contient la chaine a convertir
+ * Return: the number converted 
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int s, j, k, i;
+    s = 0;
+    for (j = 0; b[j] != '\0'; j++);
+
+    for (i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] != '0' && b[i] != '1')
+        {
+            return 0;
+        }
+        if (b[i] == '1')
+        {
+            int p = 1;
+            for (k = 0; k < (j - i - 1); k++)
+            {
+                p *= 2;
+            }
+            s += p;
+        }
+
+    }
+    return s;
+}
